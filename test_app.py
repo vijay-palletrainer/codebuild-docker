34f95115468225ac292d6f07ade88d3a08a1667e
VijayaPardhaSaradhiTummala <pardhutummalagit@gmail.com>
2025-05-18 07:17:29 +0530

    first commit

---
+++ test_app.py
@@ -0,0 +1,7 @@
+from app import app
+
+def test_home():
+    tester = app.test_client()
+    response = tester.get("/")
+    assert response.status_code == 200
+    assert b"Hello from Flask" in response.data
