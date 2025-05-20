5c85d052118353d39cb2becfdc68190d4003eb6d
vijay-palletrainer <palletrainer12@techpalle.com>
2025-05-20 16:25:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on AWS CodeBuild!"
+    return "Hello from Flask on AWS CodeBuild! first modification"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
