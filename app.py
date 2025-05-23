c858780b8b9ce2eaf2e9a378e5de532881086af3
vijay-palletrainer <palletrainer12@techpalle.com>
2025-05-23 20:46:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hey this is new website which modified from old one"
+    return "hey this is new website which modified from old one - modified"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
