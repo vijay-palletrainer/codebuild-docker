66c5df29e52b82ba8ca8d2c6393ac328d1883a54
vijay-palletrainer <palletrainer12@techpalle.com>
2025-05-23 20:08:38 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     tester = app.test_client()
     response = tester.get("/")
     assert response.status_code == 200
-    assert b"vijay" in response.data
+    assert b"hey this" in response.data
